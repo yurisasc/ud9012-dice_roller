4b0e6e09078c96ac05b2babfee8a546d5cbb115a
Yuris Aryansyah <yuris_aryansyah@yahoo.com>
2019-12-19 21:52:07 +1000

    changed the text results to dice images

--- app/src/main/java/com/cakranegara/diceroller/MainActivity.kt
+++ app/src/main/java/com/cakranegara/diceroller/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit ensures that the variable will be initialized before any calls to it
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +21,25 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
-
+        // minimize the number of findViewById calls (reduce lag) by initializing it once here
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6) + 1
 
-        resultText.text = randomInt.toString()
+        // map the resulting random integer to a drawable resource
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // diceImage could be initialized here, but it's not good for the app performance
+        // calling findViewById many times can cause lag
+        diceImage.setImageResource(drawableResource)
     }
 }
